7f798f112ed6d50348bd1a7e733300df62e5ca9b
Sandeep Kommuru <sandeepkm401@gmail.com>
2025-11-05 23:52:22 +0530

    Added requirements.txt for Render deployment

--- project/project/settings.py
+++ project/project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f^uldo_+uz*u@yv2z^!em)ilv#-mx&-(a_7sjksk6-y(*+$g=e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thegoodfind.onrender.com', 'localhost']
 
 
 # Application definition
